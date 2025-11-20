5de6dc0a24f14980c7971818fe795222942ecad8
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-20 18:34:30 +0100

    file:- for code hashing

--- middlewares/hasher.js
+++ middlewares/hasher.js
@@ -1,5 +1,14 @@
-const { hash } = require('bcryptjs');
+const bcrypt = require('bcrypt');
+const {createHmac} = require("node:crypto");
 
-exports.doHash = async (value, saltValue) => {
-    return hash(value, saltValue);
+exports.doHash = async (password, saltValue = 12) => {
+    return await bcrypt.hash(password, saltValue);
+};
+
+exports.doValidation = async (password, hashedValue) => {
+    return await bcrypt.compare(password, hashedValue);
+}
+
+exports.hmacProcess = (value, key) => {
+    return createHmac('sha256', value).update(key).digest('hex');
 }
