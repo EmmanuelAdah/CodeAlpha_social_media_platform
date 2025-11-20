0d5ff6c61306b25b3015002d2a26327f2f209488
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-20 18:34:56 +0100

    file:- for generating json web token

---
+++ middlewares/jwtGenerator.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+exports.generateToken = async (payload) =>{
+    return await jwt.sign(
+                    payload,
+                    process.env.SECRET_KEY,
+                    { expiresIn: "24h" }
+                    );
+}
+
+exports.userDetails = async (token, res) =>{
+    return await res.cookie(
+                    'Authorization', 'Bearer ' + token,
+                    { expires: new Date(Date.now() + 1000 * 60 * 60 + 24),
+                    httpOnly: process.env.NODE_ENV === 'production',
+                    secure: process.env.NODE_ENV === 'production'
+                    })
+                .json({
+                    success: true,
+                    user: token,
+                    message: 'User logged in successfully'
+                });
+}
