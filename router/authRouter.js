98223181039859420ab5b04cf04782c1f482ec40
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-20 18:35:20 +0100

    file:- API routing

--- router/authRouter.js
+++ router/authRouter.js
@@ -3,7 +3,9 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/login', authController.signin);
+router.post('/signin', authController.signin);
 router.post('/signup', authController.signup);
+router.post('/signout', authController.signout);
+router.patch('/verify/user', authController.verificationEmail);
 
 module.exports = router ;
