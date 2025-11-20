c52af90a617639d81bf7c0aa2ec1cbd8add68a99
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-20 18:35:54 +0100

    file:- for field validation

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -2,6 +2,12 @@
 const Joi = require("joi");
 
 exports.signupSchema = Joi.object({
+    username: Joi.string()
+        .min(3)
+        .max(30)
+        .pattern(/^[A-Za-z0-9_]+$/)
+        .required(),
+
     email: Joi.string()
         .min(6)
         .max(50)
@@ -12,6 +18,30 @@ exports.signupSchema = Joi.object({
         .required()
         .pattern(new RegExp(`^(?=.*[A-Za-z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{6,30}$`))
         .messages({
-            "string.pattern.base": "Password must be exactly 6 characters and include letters, numbers, and symbols."
+            "string.pattern.base": "Password must be between 6 to 30 characters and include letters, numbers, and symbols."
         })
 });
+
+exports.signinSchema = Joi.object({
+    email: Joi.string()
+    .min(6)
+    .max(50)
+    .required()
+    .email({ tlds: { allow: ['com', 'net'] }}),
+
+    password: Joi.string()
+    .required()
+})
+
+exports.verificationCodeSchema = Joi.object({
+    email: Joi.string()
+    .min(6)
+    .max(50)
+    .required()
+    .email({ tlds: { allow: ['com', 'net'] }}),
+
+    providedCode: Joi.number()
+    .required()
+})
+
+
