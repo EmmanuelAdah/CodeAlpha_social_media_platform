98a0a565b610e3d2e509e0f16c1632c7f66c95c0
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-20 18:33:39 +0100

    controller for business logics

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,51 +1,126 @@
 
-const { signupSchema} = require('../middlewares/validator');
-const { doHash } = require("../middlewares/hasher");
+const { signupSchema, verificationCodeSchema} = require('../middlewares/validator');
+const { doHash, doValidation, hmacProcess} = require("../middlewares/hasher");
 const User = require("../models/userModel");
+const { generateToken, userDetails } = require("../middlewares/jwtGenerator");
+const transport = require("../middlewares/emailSender");
+const crypto = require('crypto');
 
-exports.signin = (req, res) => {
-    res.json({'message':'This is the router in authController'});
 
-    try{
-        console.log('This is the try block');
-        res.send('This is the response' + res.status(200));
-    } catch (err){
-        console.log(err);
+exports.signin = async (req, res) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({email}).select('+password');
+    if (!existingUser) {
+        return res.status(401)
+                    .json({'message':'Invalid email or password'});
     }
+    const valid = await doValidation(password, existingUser.password);
+    if (!valid) {
+        return res.status(401)
+                    .json({'message':'Invalid email or password'});
+    }
+    const payload = {
+        'userId': existingUser.id,
+        'email': existingUser.email,
+    }
+    const token = await generateToken(payload);
+    return await userDetails(token, res);
 }
 
+
 exports.signup = async (req, res) => {
-    const { email, password } = req.body;
+    const { username, email, password } = req.body;
+
     try{
-     const {error} = signupSchema.validate({ email, password });
+     const {error} = signupSchema.validate({ username, email, password });
 
         if(error){
-             console.log(error.details[0].message);
-             return res.status(400)
-                 .send(error.details[0].message);
+             return res.status(400).send(error.details[0].message);
          }
          let existingUser = await User.findOne({ email });
 
-         if(existingUser){
-             return res.status(401).json({succes: false, 'message': 'User already exists'});
+        if(existingUser){
+             return res.status(400).json({success: false, 'message': 'User already exists'});
          }
-         const hashedPassword = doHash(password, 12);
+         const hashedPassword = await doHash(password);
 
          const savedUser = await new User({
-             email,
+             username,
+              email,
              password: hashedPassword
           }).save();
 
-        const responseUser = {
+        const userResponse = {
             id: savedUser.id,
             email: savedUser.email
         };
-        return res.status(201).json({
+        const token = await generateToken(userResponse);
+        return await userDetails(token, res);
+     } catch (err) {
+        res.status(400).json(err.message);
+    }
+}
+
+exports.signout = async (req, res) => {
+    res.clearCookie('Authorization')
+        .status(200).json({
             success: true,
-            user: responseUser
+            message: 'User logged out successfully'
+            });
+}
+
+exports.verificationEmail = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser) {
+            return res.status(404).json({ message: 'Not a verified user' });
+        }
+        const verificationCode = crypto.randomInt(100000, 999999).toString();
+
+        const sentMail = await transport.sendMail({
+            from: process.env.EMAIL_SENDER_ADDRESS,
+            to: existingUser.email,
+            subject: 'Verification code',
+            html: `<h3 xmlns="http://www.w3.org/1999/html">Your verification code is <strong>${verificationCode}</strong>.<br>Use this six digit code to reset your password</h3>`
         });
-     } catch (err){
-         console.log(err.message);
-     }
-      res.json({'message': 'This is the router in signup from authController'});
+
+        if (sentMail.accepted.includes(existingUser.email)) {
+            existingUser.verificationCode = hmacProcess(
+                verificationCode,
+                process.env.HMAC_VERIFICATION_CODE_KEY
+            );
+            await existingUser.save();
+            return res.status(200).json({ message: 'Verification code sent successfully' });
+        }
+        return res.status(500).json({ message: 'Email not delivered' })
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
+    }
+};
+
+
+exports.verifyCode = async (req, res) => {
+    const { email, codeInput } = req.body;
+
+    try{
+        const {error, value } = validationCodeSchema.validate({ email, codeInput });
+        if (error)
+            return res.status(400).send(error.details[0].message);
+
+        const existingUser = await User.findOne({ email }).select('+verificationCode');
+        const codeValue = codeInput.toString()
+
+        if (!existingUser)
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid verification code'
+            });
+        existingUser.createdAt.getTime()
+    } catch (err) {
+        res.status(400).json(err.message);
+    }
 }
