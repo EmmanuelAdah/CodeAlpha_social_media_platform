27271cc93c989004459b226ae05c78a04449fa8f
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-25 18:24:34 +0100

    file:- router

--- backend/router/postRouter.js
+++ backend/router/postRouter.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 
 
-router.get('/get/:userId', postController.getAllPosts);
+router.get('/getAll', postController.getAllPosts);
+router.get('/get/:userId', postController.getPosts);
 router.post('/create', postController.createPost);
 router.get("/getByID/:id", postController.findById);
 router.delete('/delete/:id', postController.deletePost);
